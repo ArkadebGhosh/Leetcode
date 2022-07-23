708bd09d97dcd0ed3ca0ed86173b78b135083124
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-07-23 23:33:23 +0530

    Time: 16 ms (44.77%), Space: 18 MB (6.65%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct ele {
+    TreeNode* node;
+    unsigned long long int lvl;
+    int depth;
+};
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        vector<ele> v;
+        v.push_back({root, 0, 0});
+        int currdepth = 0;
+        unsigned long long int low = 0;
+        unsigned long long int high = 0;
+        unsigned long long int ans = 1;
+        for(int i=0; i<v.size(); i++) {
+            TreeNode* curr = v[i].node;
+            unsigned long long int lvl = v[i].lvl;
+            int depth = v[i].depth;
+            if(curr->left)
+                v.push_back({curr->left, 2*lvl + 1, depth+1});
+            if(curr->right)
+                v.push_back({curr->right, 2*lvl + 2, depth+1});
+            if(depth!=currdepth) {
+                // cout<<low<<" "<<high<<endl;
+                ans = max(ans, high - low + 1);
+                low = lvl;
+                currdepth = depth;
+            }
+            high = lvl;
+        }
+        ans = max(ans, high - low + 1);
+        return (int)ans;
+    }
+};
