d2ca690393d45bada0133f8e19123e81cf8c2a46
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-07-29 16:35:51 +0530

    Time: 181 ms (15.77%), Space: 35.2 MB (61.29%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -6,11 +6,11 @@ public:
         
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> temp = nums;
-        int m1 = maxmin(temp, 3, n-1);
-        int m2 = maxmin(temp, 0, n-4);
-        int m3 = maxmin(temp, 2, n-2);
-        int m4 = maxmin(temp, 1, n-3);
+        
+        int m1 = maxmin(nums, 3, n-1);
+        int m2 = maxmin(nums, 0, n-4);
+        int m3 = maxmin(nums, 2, n-2);
+        int m4 = maxmin(nums, 1, n-3);
         return min(m1, min(m2, min(m3, m4)));
     }
     
