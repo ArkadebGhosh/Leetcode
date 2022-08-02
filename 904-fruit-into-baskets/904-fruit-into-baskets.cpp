8b1100aa4c8e3974bfb30cb4359a8d8b1a191b1a
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-08-03 04:04:54 +0530

    Time: 145 ms (94.44%), Space: 66.2 MB (93.52%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int ans = 0;
+        int currans = 0;
+        int newpick = -1;
+        int oldpick = -1;
+        
+        for(int i = 0; i<n; i++) {
+            if(newpick == -1) {
+                newpick = i;
+                ++currans;
+            }
+            else if(fruits[i] == fruits[newpick]) {
+                newpick = i;
+                ++currans;
+            }
+            else if(oldpick == -1) {
+                oldpick = newpick;
+                newpick = i;
+                ++currans;
+            }
+            else if(fruits[i] == fruits[oldpick]) {
+                oldpick = newpick;
+                newpick = i;
+                ++currans;
+            }
+            else {
+                ans = max(ans, currans);
+                currans = i - oldpick;
+                oldpick = newpick;
+                newpick = i;
+            }
+        }
+        ans = max(ans, currans);
+        return ans;
+    }
+};
