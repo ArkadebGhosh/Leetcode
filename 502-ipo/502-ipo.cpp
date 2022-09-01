bb9cb44ff2921c3d14a3a73c5086353ed2d30e52
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-09-01 06:19:17 +0530

    Time: 460 ms (26.36%), Space: 82.1 MB (39.95%) - LeetHub

---
+++ 502-ipo/502-ipo.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        priority_queue<int> pq;
+        vector<pair<int, int>> arr;
+        for(int i = 0; i<profits.size(); i++)
+            arr.push_back({capital[i], profits[i]});
+        sort(arr.begin(), arr.end());
+        int funds = w;
+        int i = 0;
+        int n = profits.size();
+        if(arr[0].first > w)
+            return w;
+        
+        while(i<n && k>0) {
+            // cout<<arr[i].first<<" "<<arr[i].second<<endl;
+            if(arr[i].first <= funds) {
+                pq.push(arr[i].second);
+                ++i;
+            }
+            else {
+                while(!pq.empty() && k>0 && funds < arr[i].first) {
+                    funds+=pq.top();
+                    pq.pop();
+                    --k;
+                }
+                if(funds < arr[i].first)
+                    break;
+            }
+            // cout<<funds<<endl;
+        }
+        
+        while(k>0 && !pq.empty()) {
+            funds+=pq.top();
+            pq.pop();
+            --k;
+        }
+        return funds;
+    }
+};
