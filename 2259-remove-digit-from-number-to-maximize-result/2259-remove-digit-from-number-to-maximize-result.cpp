eeb2082fb23f781f5d7cf4add714f75eaff93433
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-10-15 23:30:07 +0530

    Time: 3 ms (64.59%), Space: 6.2 MB (72.74%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     string removeDigit(string number, char digit) {
-        vector<string> possible;
-        for(int i = 0; i<number.size(); i++) {
-            if(number[i] == digit) {
-                string str = "";
-                for(int j = 0; j < number.size(); j++) {
-                    if(j!=i)
-                        str+=number[j];
-                }
-                possible.push_back(str);
+        // vector<string> possible;
+        // for(int i = 0; i<number.size(); i++) {
+        //     if(number[i] == digit) {
+        //         string str = "";
+        //         for(int j = 0; j < number.size(); j++) {
+        //             if(j!=i)
+        //                 str+=number[j];
+        //         }
+        //         possible.push_back(str);
+        //     }
+        // }
+        // sort(possible.begin(), possible.end());
+        // return possible.back();
+        int n = number.size();
+        int idx = -1;
+        int lastidx = 0;
+        for(int i = 0; i<n; i++) {
+            if(i<n-1 && number[i] == digit && number[i+1] > number[i]) {
+                idx = i;
+                break;
             }
+            if(number[i] == digit)
+                lastidx = i;
         }
-        sort(possible.begin(), possible.end());
-        return possible.back();
+        if(idx == -1)
+            idx = lastidx;
+        string ans = "";
+        for(int i = 0; i<n; i++) {
+            if(i!=idx)
+                ans+=number[i];
+        }
+        return ans;
     }
 };
