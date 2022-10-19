867c819e034b695364e7a793b9fc78b4f40069fa
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-10-19 20:21:27 +0530

    Time: 8 ms (87.24%), Space: 11 MB (98.00%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -5,18 +5,7 @@ public:
     {
         string a = to_string(x);
         string b = to_string(y);
-        
-        if (a.size() > b.size() && a.compare(0, b.size(), b) == 0) {
-            string x = a.substr(b.size());
-            return (x + b) < (b + x);
-        }
-        
-        if (a.size() < b.size() && b.compare(0, a.size(), a) == 0) {
-            string x = b.substr(a.size());
-            return (x + a) > (a + x);
-        }
-        
-        else return a < b;
+        return a + b < b + a;
     }
     
     string largestNumber(vector<int>& nums) {
