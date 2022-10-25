76c7378c3909076e1796ac2b2a1aa856516e6e7f
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-10-25 16:43:41 +0530

    Time: 461 ms (50.09%), Space: 70.6 MB (86.00%) - LeetHub

---
+++ 0715-range-module/0715-range-module.cpp
@@ -0,0 +1,124 @@
+class RangeModule {
+public:
+    map<int, int> m; // first -> Begin, second -> End
+    RangeModule() {
+        m.clear();
+    }
+    void print() {
+        // for(auto &it: m) {
+        //     cout<<"("<<it.first<<":"<<it.second<<") ";
+        // }
+        // cout<<endl;
+        return;
+    }
+    
+    void addRange(int left, int right) {
+        // cout<<"Added: "<<left<<" "<<right<<endl;
+        if(m.size() == 0) {
+            m[left] = right - 1;
+            print();
+            return;
+        }            
+        removeRange(left, right);
+        --right;
+        // print();
+        map<int, int>:: iterator it, it2;
+        it = m.lower_bound(left);
+        it2 = it;
+        if(it == m.begin()) {
+            m[left] = right;
+        }
+        else {
+            --it;
+            int low = (*it).first;
+            int high = (*it).second;
+            // cout<<low<<" "<<high<<endl;
+            if(high == left-1) {
+                m[low] = max(high, right);
+                left = low;
+            }
+            else {
+                m[left] = right;
+            }
+        }
+        if(it2 == m.end()) {
+            print();
+            return;
+        }
+            // return;
+        if((*it2).first == right+1 ) {
+            m[left] = (*it2).second;
+            m.erase((*it2).first);
+        }
+        print();
+    }
+    
+    bool queryRange(int left, int right) {
+        // cout<<"Query: "<<left<<" "<<right<<endl;
+        --right;
+        print();
+        map<int, int>:: iterator it = m.lower_bound(left);
+        if(((*it).first) == left && ((*it).second) >= right) {
+            return true;
+        }
+        else if((*it).first > left && (*it).first <= right) {
+            return false;
+        }
+        else if (it != m.begin()) {
+            --it;
+            if((*it).second >= right)
+                return true;
+        }
+        return false;
+    }
+    
+    void removeRange(int left, int right) {
+        // cout<<"Removed: "<<left<<" "<<right<<endl;
+        if(m.size() == 0)
+            return;
+        --right;
+        map<int, int>:: iterator it, it2;
+        it= m.lower_bound(left);
+        it2 = it;
+        while(it != m.end()) {
+            int low = (*it).first;
+            int high = (*it).second;
+            ++it;
+            // cout<<"Erased "<<low<<" "<<high<<endl;
+            if(low > right)
+                break;
+            if(high <= right) {
+                m.erase(low);
+            }
+            else if(high > right) {
+                m.erase(low);
+                m[right+1] = high;
+            }
+            // ++it;
+        }
+        it2 = m.lower_bound(left);
+        if(it2 == m.begin()) {
+            print();
+            return;
+        }
+            // return;
+        --it2;
+        int low = (*it2).first;
+        int high = (*it2).second;
+        if(high >= left) {
+            m[low] = left - 1;
+        }
+        if(high > right) {
+            m[right+1] = high;
+        }
+        print();
+    }
+};
+
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * RangeModule* obj = new RangeModule();
+ * obj->addRange(left,right);
+ * bool param_2 = obj->queryRange(left,right);
+ * obj->removeRange(left,right);
+ */
