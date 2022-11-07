84c1d2897a9940ea20ef0172625ca4862449f26b
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-11-08 03:35:41 +0530

    Time: 1866 ms (12.41%), Space: 397.6 MB (8.39%) - LeetHub

---
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+        unordered_map<int, int> ht;
+        unordered_map<int, priority_queue<pair<int, int>>> dp, dp2;
+        unordered_map<int, int> dpth;
+        height(root, 0, ht, dp, dpth);
+        dp2 = dp;
+        vector<int> ret;
+        // for(auto &it: dp2) {
+        //     cout<<"At depth = "<<it.first<<endl;
+        //     while(!it)
+        // }
+        for(auto &query: queries) {
+            int ans = ht[root->val] - ht[query];
+            int depth = dpth[query];
+            int greatest_at_depth = dp[depth].top().second;
+            if(greatest_at_depth != query) {
+                ans = ht[root->val];
+            }
+            else {
+                pair<int, int> temp = dp[depth].top();
+                dp[depth].pop();
+                if(dp[depth].size() != 0) 
+                    ans = ans + dp[depth].top().first;
+                dp[depth].push(temp);
+            }
+            ret.push_back(ans-1);
+        }
+        return ret;
+    }
+    
+    int height(TreeNode* root, int depth, unordered_map<int, int> &ht, unordered_map<int, priority_queue<pair<int, int>>> &dp, unordered_map<int, int> &dpth) {
+        if(root->left == NULL && root->right == NULL) {
+            dp[depth].push({1, root->val});
+            ht[root->val] = 1;
+            dpth[root->val] = depth;
+            return 1;
+        }
+        int lh = 0, rh = 0;
+        dpth[root->val] = depth;
+        if(root->left)
+            lh = height(root->left, depth+1, ht, dp, dpth);
+        if(root->right)
+            rh = height(root->right, depth+1, ht, dp, dpth);
+        ht[root->val] = 1 + max(lh, rh);
+        dp[depth].push({ht[root->val], root->val});
+        return ht[root->val];
+    }
+};
